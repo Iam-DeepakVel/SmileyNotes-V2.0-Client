d2bea38479be5e87cb1065472ba5fb48591e0237
iam-deepakvel <deepakvel82@gmail.com>
2023-04-15 17:44:21 +0530

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import reduxStore from "@/store/store";
+import dynamic from "next/dynamic";
+import NextNProgress from "nextjs-progressbar";
+import { motion } from "framer-motion";
+
+const Navbar = dynamic(() => import("@/components/Navbar"), {
+  ssr: false,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <motion.div>
+      <Provider store={reduxStore}>
+        <Navbar />
+        <NextNProgress
+          color="#ba882d"
+          startPosition={0.3} 
+          stopDelayMs={300}
+          height={4}
+          showOnShallow={true}
+        />
+        <Component {...pageProps} />
+      </Provider>
+    </motion.div>
+  );
 }
